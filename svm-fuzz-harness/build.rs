8d9b7495a3d1ab52e9a8921006ca5ff10a6e2443
Charles Moyes <cmoyes@jumptrading.com>
2025-10-13 14:49:50 +0000

    normalize for windows

--- svm-fuzz-harness/build.rs
+++ svm-fuzz-harness/build.rs
@@ -1,13 +1,36 @@
-use std::{env, fs, path::PathBuf};
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+};
+
+fn maybe_normalize_windows_path(path: &Path) -> PathBuf {
+    #[cfg(windows)]
+    {
+        // UNC paths are not supported by prost_build, so we need to re-normalize them.
+        const EXTENDED_PREFIX: &str = r"\\?\";
+        let s = path.display().to_string();
+        if s.starts_with(EXTENDED_PREFIX) {
+            PathBuf::from(&s[EXTENDED_PREFIX.len()..])
+        } else {
+            path.to_path_buf()
+        }
+    }
+
+    #[cfg(not(windows))]
+    {
+        path.to_path_buf()
+    }
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Get absolute proto dir from producer
-    let proto_dir = PathBuf::from(
+    let proto_dir = maybe_normalize_windows_path(&PathBuf::from(
         env::var("DEP_PROTOSOL_PROTO_DIR")
-            .expect("protosol did not expose PROTO_DIR"),
-    );
+            .expect("protosol did not expose PROTO_DIR, did protosol build.rs run first?"),
+    ));
 
     println!("cargo:rerun-if-env-changed=DEP_PROTOSOL_PROTO_DIR");
+    println!("cargo:rerun-if-changed={}", proto_dir.display());
 
     // Collect absolute .proto paths
     let mut proto_files = vec![];
@@ -19,13 +42,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
+    // Ensure deterministic order for rebuilds
+    proto_files.sort();
+
     // Compile protos into Rust
     let out_dir = PathBuf::from(env::var("OUT_DIR")?);
     let mut config = prost_build::Config::new();
     config.out_dir(&out_dir);
 
     config.compile_protos(
-        &proto_files.iter().map(|p| p.display().to_string()).collect::<Vec<_>>(),
+        &proto_files
+            .iter()
+            .map(|p| p.display().to_string())
+            .collect::<Vec<_>>(),
         &[proto_dir.to_str().unwrap()],
     )?;
 
